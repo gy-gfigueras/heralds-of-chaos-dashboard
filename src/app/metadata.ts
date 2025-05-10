62fba2aea01c6d56c0c26dc606e1b5c52c0ede27
gy-gfigueras <willstar111yt@gmail.com>
2025-05-10 21:28:03 +0200

    feat: add metadata

---
+++ src/app/metadata.ts
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Heralds of Chaos',
+  description:
+    'Explore the world of Heralds of Chaos, a rich universe filled with characters, worlds, items, and creatures.',
+  icons: {
+    icon: '/icons/hoc.ico',
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+  },
+};
